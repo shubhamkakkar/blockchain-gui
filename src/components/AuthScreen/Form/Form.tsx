0be39382902d665def28f4213e6f7c16ddb57966
shubhamkakkar <shubham.kakkar998@gmail.com>
2019-10-17 12:36:23 +0530

    working on form container

--- src/components/AuthScreen/Form/Form.tsx
+++ src/components/AuthScreen/Form/Form.tsx
@@ -2,10 +2,34 @@ import React from 'react';
 import classes from './Form.module.scss';
 
 
+type TLoginForm = {
+    type?: string,
+    label: string
+}
+
+const loginForm: TLoginForm[] = [
+    {
+        label: "Email",
+        type: "email"
+    },
+    {
+        label: "Password",
+        type: "password"
+    }
+]
+
+
 export default function Form() {
     return (
         <div className={classes.formContainer}>
-
+            <div className={classes.formChild}>
+                First name:
+                <input type="text" name="firstname" />
+            </div>
+            <div>
+                Last name:
+                <input type="text" name="lastname" />
+            </div>
         </div>
     )
 }
