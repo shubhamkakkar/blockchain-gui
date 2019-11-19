65e2b0680b644e12d0ec1863e81060de652ffc2e
shubhamkakkar <shubham.kakkar998@gmail.com>
2019-11-20 02:10:52 +0530

    blocks - Responsive

--- src/components/Blocks/BlocksCard.tsx
+++ src/components/Blocks/BlocksCard.tsx
@@ -1,46 +1,45 @@
 import React from "react";
 import classes from "./scss/BlocksCard.module.scss";
-import { Block } from "../../generated/graphql";
+import {Block} from "../../generated/graphql";
 
 type TData = {
-  blocks: Block[];
+    blocks: Block[];
 };
 
-function CardRow({ label, value }: { label: string; value: string }) {
-  return (
-    <div className={classes.cardRow}>
-      <div className={classes.label}>{label}</div>
-      <div className={classes.value}>{value}</div>
-    </div>
-  );
+function CardRow({label, value}: { label: string; value: string }) {
+    return (
+        <div className={classes.cardRow}>
+            <div className={classes.label}>{label} : </div>
+            <div className={classes.value}>{value}</div>
+        </div>
+    );
 }
 
 function BlockCard({
-  blockInfo: { _id, password, index, hash, data }
-}: {
-  blockInfo: Block;
+                       blockInfo: {_id, password, index, hash, data, prevHash}
+                   }: {
+    blockInfo: Block;
 }) {
-  return (
-    <div className={classes.blockCard}>
-      <CardRow label={"Block Hash"} value={hash} />
-      <CardRow label={"Digital Password"} value={password} />
-    </div>
-  );
+    return (
+        <div className={classes.blockCard}>
+            <CardRow label={"Previous hash"} value={prevHash}/>
+            <CardRow label={"Block Hash"} value={hash}/>
+            <CardRow label={"Digital Password"} value={password}/>
+        </div>
+    );
 }
 
 export default function BlocksCard({
-  data,
-  error
-}: {
-  data: TData;
-  error: any;
+                                       data: {blocks},
+                                       error
+                                   }: {
+    data: TData;
+    error: any;
 }) {
-  return (
-    <div className={classes.blockCardContainer}>
-      {error && <div>error : {error.message}</div>}
-      {data.blocks.map((blockInfo: Block, key: number) => (
-        <BlockCard {...{ blockInfo, key }} />
-      ))}
-    </div>
-  );
+    return (
+        <div className={classes.blockCardContainer}>
+            {error && <div>error : {error.message}</div>}
+            {blocks.map((blockInfo: Block, key: number) => <BlockCard {...{blockInfo, key}} />)}
+        </div>
+    );
 }
