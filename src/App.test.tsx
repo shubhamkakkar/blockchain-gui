a5e1fbf02e8d16c0ef8fe74676955324468ac475
shubhamkakkar <shubham.kakkar998@gmail.com>
2019-11-24 20:47:45 +0530

    app.test.tsx written

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import Enzyme, {shallow} from "enzyme"
-import EnzymeAdapter from "enzyme-adapter-react-16"
+import React from "react";
+import Enzyme, { shallow, ShallowWrapper } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-import App from './App';
 
-Enzyme.configure({adapter: new EnzymeAdapter()})
+import App, { routes } from "./App";
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+
+const findByAttr = (wrapper: ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>, value: string) =>
+  wrapper.find(`[data-test="${value}"]`)
 
 describe("render app.tsx", () => {
-    let wrapper: Enzyme.ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
-    beforeEach(() => {
-        wrapper = shallow(<App/>)
-    });
-    it('wrapper must be found', () => {
-        expect(wrapper.length).toBe(1)
-    });
-
+  let wrapper: ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+  it("wrapper must be found", () => {
+    expect(wrapper.length).toBe(1);
+  });
+  it("RouterContainer have children", () => {
+    const RouterContainer = findByAttr(wrapper, "RouterContainer")
+    expect(RouterContainer).toHaveLength(1);
+  });
+  it("SwitchContainer have children", () => {
+    const SwitchContainer = findByAttr(wrapper, "SwitchContainer")
+    expect(SwitchContainer).toHaveLength(1);
+  });
+  it("Route have children", () => {
+    const Route = findByAttr(wrapper, "Route")
+    expect(Route).toHaveLength(routes.length);
+  });
 });
-
