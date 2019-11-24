a70ea3d2bb651d580eb4156268a79032cebb61db
shubhamkakkar <shubham.kakkar998@gmail.com>
2019-11-24 20:10:25 +0530

    wrapper exists

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, {shallow} from "enzyme"
+import EnzymeAdapter from "enzyme-adapter-react-16"
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({adapter: new EnzymeAdapter()})
+
+describe("render app.tsx", () => {
+    let wrapper: Enzyme.ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+    beforeEach(() => {
+        wrapper = shallow(<App/>)
+    });
+    it('wrapper must be found', () => {
+        expect(wrapper.length).toBe(1)
+    });
+
 });
+
